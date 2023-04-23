e24c9ea2ca2fdddc565176f2102939fe26740f14
nocyphr <nocyphr@outlook.com>
2023-04-23 16:14:54 +0200

    fix: added sleeper for debuggin behave

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -1,8 +1,12 @@
 from crud import CRUD
 from misc import *
+from time import sleep
 
 if __name__ == "__main__":
     data = load_data_from_file('/app/assets/data.json')
     data_dict = convert_json_to_data_dict(data)
     db = CRUD()
     db.create('test', data_dict)
+
+    sleep(99999999)
+
