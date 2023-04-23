a6713dec4d7fa2c969508a48a7724161cfe623c6
nocyphr <nocyphr@outlook.com>
2023-04-23 16:14:33 +0200

    8 steps still broken

--- features/steps/steps_crud.py
+++ features/steps/steps_crud.py
@@ -4,8 +4,6 @@ from behave import given, when, then
 from src.crud import CRUD
 from src.misc import *
 
-
-
 @given(u'the database contains the following test data')
 def step_impl(context):
     global crud
@@ -19,7 +17,6 @@ def step_impl(context):
     data_dict = convert_json_to_data_dict(data)
     crud.create('test', data_dict)
 
-
 @given(u'an existing table "{table_name}"')
 def step_impl(context, table_name):
     global crud
@@ -27,7 +24,6 @@ def step_impl(context, table_name):
         crud = CRUD()
 
     assert table_name in crud.show_tables()
-
 
 @when(u'I call the insert method with the data')
 def step_impl(context):
@@ -40,24 +36,20 @@ def step_impl(context):
 
     crud.insert('test', data_dict)
 
-
 @then(u'the data should be inserted into the specified table')
 def step_impl(context):
     inserted_data = crud.read('test')
-    for row in context.table:
-        assert row in inserted_data
-
+    row = context.active_outline
+    assert row in inserted_data
 
 @when(u'I call the read method with the condition "Symbol = \'{symbol}\'"')
 def step_impl(context, symbol):
     context.returned_data = crud.read('test', f"Symbol = '{symbol}'")
 
-
 @then(u'the data returned should be')
 def step_impl(context):
-    for row in context.table:
-        assert row in context.returned_data
-
+    row = context.active_outline
+    assert row in context.returned_data
 
 @when(u'I call the update method with the set_data and the condition "Symbol = \'{symbol}\'"')
 def step_impl(context, symbol):
@@ -70,30 +62,30 @@ def step_impl(context, symbol):
 
     crud.update('test', set_data, f"Symbol = '{symbol}'")
 
-
 @then(u'the data in the specified table should be updated')
 def step_impl(context):
     updated_data = crud.read('test')
-    for row in context.table:
-        assert row in updated_data
-
+    row = context.active_outline
+    assert row in updated_data
 
 @when(u'I call the delete method with the condition "Symbol = \'{symbol}\'"')
 def step_impl(context, symbol):
+    context.symbol = symbol
     crud.delete('test', f"Symbol = '{symbol}'")
 
 
 @then(u'the data with the specified symbol should be deleted from the table')
 def step_impl(context):
     deleted_data = crud.read('test')
-    for row in context.table:
-        assert row not in deleted_data
+    symbol = context.symbol
+    for row in deleted_data:
+        assert row['Symbol'] != symbol
+
 
 
 @when(u'I call the drop_table method')
 def step_impl(context):
     crud.drop_table('test')
-
 
 @then(u'the specified table should be dropped from the database')
 def step_impl(context):
